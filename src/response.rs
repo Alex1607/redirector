23e1f4b305afb4f61da96290c4cb6402e9c0d2ba
Alexander <alex@pluoi.com>
2023-07-24 23:57:13 +0200

    Initial Commit

--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::collections::LinkedList;
 use worker::{Cors, Headers, Response};
 
 fn build_headers() -> worker::Result<Headers> {
@@ -7,30 +8,33 @@ fn build_headers() -> worker::Result<Headers> {
     Ok(headers)
 }
 
-pub fn build_response(
-    logger_response: LoggerResponse,
-    status_code: u16,
-) -> worker::Result<Response> {
-    Response::ok(serde_json::to_string(&logger_response).unwrap())?
+pub fn build_response(response: RedirectorResponse, status_code: u16) -> worker::Result<Response> {
+    Response::ok(serde_json::to_string(&response).unwrap())?
         .with_headers(build_headers()?)
         .with_status(status_code)
         .with_cors(&Cors::new().with_origins(vec!["*"]))
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct LoggerResponse {
-    #[serde(rename = "response")]
-    pub response_type: ResponseType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub details: Option<String>,
+pub struct RedirectorResponse {
+    #[serde(rename = "resultUrl", skip_serializing_if = "Option::is_none")]
+    pub result_url: Option<String>,
+
+    #[serde(rename = "status")]
+    pub response_status: ResponseType,
+
+    #[serde(rename = "urls", skip_serializing_if = "Option::is_none")]
+    pub redirect_urls: Option<LinkedList<String>>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum ResponseType {
     #[serde(rename = "BAD_REQUEST")]
     BadRequest,
+
     #[serde(rename = "OK")]
     Ok,
-    #[serde(rename = "IPLOGGER_DETECTED")]
-    IpLoggerDetected,
+
+    #[serde(rename = "URL_MALFORMED")]
+    UrlMalformed,
 }
